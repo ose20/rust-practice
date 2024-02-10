183fb4d91374ff5d69b4eb894064d3f35b050977
ose20 <ose20dive@gmail.com>
2024-02-11 00:43:02 +0900

    fix: wcr

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = wcr::get_args().and_then(wcr::run) {
+    if let Err(e) = wcr::get_config().and_then(wcr::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
